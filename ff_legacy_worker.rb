d0476c16a5c219fc269e12782832901e865b7ff5
Javier Vazquez <javier@vazquez.ch>
2013-02-12 14:21:43 +0100

    added color profiles, worked on image conversion.
    Signed-off-by: Javier Vazquez <javier@vazquez.ch>

--- ff_legacy_worker.rb
+++ ff_legacy_worker.rb
@@ -3,29 +3,12 @@
 # == Synopsis 
 #   Renames PDFs in directories to FutureFolio standard and creates a JPG thumbnail for every PDF
 #
-# == Examples
-#   ff_legacy_worker.rb -smd <directory containing SMD PDFs>
-
-#   This would recursively parse the <directory> and apply itself to PDFs in subdirectories
-#
-# == Usage 
-#   ff_legacy_worker.rb [options] directory
-#
-#   For help use: ff_legacy_worker.rb -h
-#
-# == Options
-#   -s, --smd           Work on SMD legacy PDFs
-#   -r, --ringier       Work on Ringier legacy PDFs
-#   -h, --help          Displays help message
-#   -v, --version       Display the version, then exit
-#   -q, --quiet         Output as little as possible, overrides verbose
-#   -V, --verbose       Verbose output
-#
 # == Author
 #   Javier Vazquez
 #
 # == Copyright
 #   Copyright 2013 Ringier AG, Javier Vazquez
+#
 # == License
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -43,7 +26,6 @@ require 'RMagick'
 require 'date'
 
 class App
-  VERSION = '1.0'
   
   def initialize(args, stdin)
     
@@ -61,7 +43,7 @@ class App
       # Options available for this application
       opt :smd, "Work on SMD legacy PDFs"
       opt :ringier, "Work on Ringier legacy PDFs"
-      opt :dir, "Working directory", :type => :string
+      opt :dir, "Working directory containing issue directories", :type => :string
     end
     
   end
@@ -71,19 +53,61 @@ class App
     start_time = Time.now
     validate_opts
     
-    parse_dir(@opts[:dir])
+    gather_working_dirs(@opts[:dir]).each do |issue_dir|
+      work_on_pdfs(issue_dir)
+    end
     
     puts "\nProcess took #{Time.now - start_time} seconds"      
   end
   
   protected
   
-  def parse_dir(dir)
-    working_dirs = []
-    Dir.foreach(dir) {|x| working_dirs << x.class }
-    puts working_dirs
+  def work_on_pdfs(issue_dir)
+    my_pdfs = []
+    Dir.foreach(issue_dir) {|pdf| my_pdfs << "#{issue_dir}/#{pdf}" if valid_pdf?(issue_dir, pdf) }
+    my_pdfs.each do |my_pdf|
+      puts "Working on #{my_pdf}"
+      create_thumbnail(my_pdf)
+    end
   end
   
+  def gather_working_dirs(dir)
+    working_dirs = []
+    # gather all directories within dir
+    Dir.foreach(dir) {|d| working_dirs << "#{dir}/#{d}" if valid_working_dir?(dir, d) }
+    return working_dirs
+  end
+  
+  def create_thumbnail(my_pdf)
+    pdf = Magick::ImageList.new(my_pdf)
+    thumb = pdf.resize_to_fit(256, 256)
+    thumb.strip!
+    thumb.add_profile("./lib/CoatedGRACoL2006.icc")
+    thumb.colorspace = Magick::SRGBColorspace
+    thumb.add_profile("./lib/AppleRGB.icc")
+    thumb.format = 'JPG'
+    thumb.sharpen(0,0.8)
+    thumb.write("#{File.dirname(my_pdf)}/#{File.basename(my_pdf, '.pdf')}.jpg") { self.quality = 100 }
+  end
+  
+  # check if we're looking at a directory and if directory is not . or ..
+  def valid_working_dir?(base_dir, dir)
+    regex_str = "#{base_dir}\/(\.\.|\.)"
+    working_dir = "#{base_dir}/#{dir}"
+    is_directory = File.directory?("#{base_dir}/#{dir}")
+    is_real_dir = true unless working_dir =~ /.*\/(\.\.|\.)/
+    return is_directory && is_real_dir
+  end
+  
+  # check if we're looking at a valid PDF
+  def valid_pdf?(base_dir, my_file)
+    is_file = File.file?("#{base_dir}/#{my_file}")
+    # FIXME check if file is actually a PDF
+    is_pdf = true if my_file =~ /.*.(pdf|PDF)/ 
+    return is_file && is_pdf
+  end
+  
+  # validate command-line options
   def validate_opts
     Trollop::die :dir, "must be defined" if @opts[:dir].empty?
     Trollop::die :dir, "must be a valid directory" unless File.directory?(@opts[:dir])
